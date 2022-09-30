608140457429ea059beae037610a5d2c65889f05
John <jmmcmaster09@gmail.com>
2022-09-29 20:28:03 -0500

    Studio push

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,56 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
 
+    }
+    @Test
+    public void backwardsBracketsReturnFalse() {
+        assertFalse (BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void justOpeningReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void justClosingReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void balancedBracketsWithNonBracketCharactersReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void twoSetsOfBalancedBracketsWithNonBracketCharactersReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launch][code]");
+    }
+    @Test
+    public void twoNestedSetsOfBalancedBracketsWithNonBracketCharactersReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Laun[ch]code]");
+    }
+
+    @Test
+    public void misNestedSetsOfBalancedBracketsWithNonBracketCharactersReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch]code]");
+    }
+
+    @Test
+    public void extraOpeningBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch[code]");
+    }
+
+    @Test
+    public void extraClosingBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch]code]");
+    }
+
+    @Test
+    public void twoSetsMisNestedBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Launch[Code"))
+    }
 }
